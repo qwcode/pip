6826f0bfaafede6675802d67e69f3183c19f2533
Marcus Smith <qwcode@gmail.com>
2012-08-29 19:18:46 -0700

    walk the files

--- tests/test_pip.py
+++ tests/test_pip.py
@@ -479,7 +479,9 @@ class FastTestPipEnvironment(TestPipEnvironment):
         self.use_distribute = os.environ.get('PIP_TEST_USE_DISTRIBUTE', False)
 
         if self.root_path.exists:
+            print "walking..."
             for dirpath, dirnames, filenames in os.walk(self.root_path):
+                print str(dirnames)
                 for f in filenames:
                     print dirpath, f
             rmtree(self.root_path)
