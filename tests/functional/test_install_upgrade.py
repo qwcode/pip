147f798a691b92a1ddc8047f7288165f2183e38a
Marcus Smith <qwcode@gmail.com>
2013-12-13 23:08:07 -0800

    make upgrade output verbose

--- tests/functional/test_install_upgrade.py
+++ tests/functional/test_install_upgrade.py
@@ -270,7 +270,7 @@ class TestUpgradeSetuptools(object):
         result = self.script.run(self.ve_bin/'pip', 'list')
         "setuptools (0.9.6)" in result.stdout
         "distribute (0.7.3)" in result.stdout
-        result = self.script.run(self.ve_bin/'pip', 'install', '--no-index', '--find-links=%s' % data.find_links, '-U', 'setuptools')
+        result = self.script.run(self.ve_bin/'pip', 'install', '--no-index', '-vv', '--find-links=%s' % data.find_links, '-U', 'setuptools')
         assert "Found existing installation: setuptools 0.9.6" in result.stdout
         result = self.script.run(self.ve_bin/'pip', 'list')
         "setuptools (0.9.8)" in result.stdout
